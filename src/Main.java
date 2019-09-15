e95dcfc2d4e1e3126a3bdef7ba7cd66121ec8fd5
student <student@localhost>
2019-09-15 09:40:16 +0300

    executed task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        int purchaseAmount = 16_500; // сумма всех покупок
+        int amountPerPurchase = 1_900; // сумма одной покупки
+        int bonuses; // объявляем переменную начисленных бонусов
+        int deliteli = 1_000;
+        int border = 15_000;
+        if (purchaseAmount <= border) {
+            bonuses = (amountPerPurchase / deliteli) * 50;
+            System.out.println(bonuses);
+        }
+        else if (purchaseAmount > border && purchaseAmount <= 150_000) {
+            bonuses = (amountPerPurchase / deliteli) * 70;
+            System.out.println(bonuses);
+        }
+        else {
+            bonuses = (amountPerPurchase / deliteli) * 100;
+            System.out.println(bonuses);
+        }
 
     }
 }
+
