e134c648869524ccdef4588a085675a51327f872
student <student@localhost>
2019-09-15 10:13:54 +0300

    upgraded task, added keyboard input

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,30 @@
+import java.util.Scanner; // вызываем инструменты ввода с клавиатуры (эту вещь нашел нашел в интернете и применил в задаче)
 public class Main {
     public static void main(String[] args) {
-        int purchaseAmount = 16_500; // сумма всех покупок
-        int amountPerPurchase = 1_900; // сумма одной покупки
-        int bonuses; // объявляем переменную начисленных бонусов
+        Scanner vvod = new Scanner(System.in);  //создаём инструмент пользовательского ввода с клавиатуры
+        int purchaseAmount = 150_001; // создаем переменную - сумма всех покупок
+        int amountPerPurchase; // объявляем переменную - сумма одной покупки
+        int bonuses; // объявляем переменную  - начисленные бонусы
         int deliteli = 1_000;
         int border = 15_000;
+        System.out.print("Введите сумму Вашей покупки в рублях: ");  //выводим на консоль сообщение о вводе
+        amountPerPurchase = vvod.nextInt();  //вводим значение в созданную переменную
+        System.out.println("Сумма Вашей покупки: " + amountPerPurchase + " рублей"); //выводим отформатированное сообщение
+        // содержащее значение введённой переменной
+
+// далее рассчитываем сумму начсиленных бонусов
+
         if (purchaseAmount <= border) {
             bonuses = (amountPerPurchase / deliteli) * 50;
-            System.out.println(bonuses);
+            System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
         else if (purchaseAmount > border && purchaseAmount <= 150_000) {
             bonuses = (amountPerPurchase / deliteli) * 70;
-            System.out.println(bonuses);
+            System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
         else {
             bonuses = (amountPerPurchase / deliteli) * 100;
-            System.out.println(bonuses);
+            System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
 
     }
