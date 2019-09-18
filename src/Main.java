3521375cd7f1b563ac61e82338c5dc8c7d2d7b8e
student <student@localhost>
2019-09-18 19:49:07 +0300

    #2, #3 executed task

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,23 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int purchaseAmount = 150_001;
         int amountPerPurchase;
-        int bonuses;
-        int deliteli = 1_000;
+        int divider = 1_000;
         int border = 15_000;
         System.out.print("Введите сумму Вашей покупки в рублях: ");
         amountPerPurchase = input.nextInt();
         System.out.println("Сумма Вашей покупки: " + amountPerPurchase + " рублей");
 
+        int bonuses;
         if (purchaseAmount <= border) {
-            bonuses = (amountPerPurchase / deliteli) * 50;
+            bonuses = (amountPerPurchase / divider) * 50;
             System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
         else if (purchaseAmount > border && purchaseAmount <= 150_000) {
-            bonuses = (amountPerPurchase / deliteli) * 70;
+            bonuses = (amountPerPurchase / divider) * 70;
             System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
         else {
-            bonuses = (amountPerPurchase / deliteli) * 100;
+            bonuses = (amountPerPurchase / divider) * 100;
             System.out.println("Сумма начисленных бонусов составит: " + bonuses);
         }
 
